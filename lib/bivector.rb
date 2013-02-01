02957d3ebfe94caa2a378797404fb7e43422434e
hegwin <zwt315@163.com>
2013-02-01 16:43:33 +0800

    add some basic methods for vector

--- lib/bivector.rb
+++ lib/bivector.rb
@@ -7,7 +7,38 @@ class Bivector
 
   def +(another)
     if another.is_a? Bivector
-      Bivector.new(self.x + another.x, self.y + another.y)
+      Bivector.new(x + another.x, y + another.y)
     end
+  end
+
+  def -(another)
+    if another.is_a? Bivector
+      Bivector.new(x - another.x, y - another.y)
+    end
+  end
+
+  def *(aNumber)
+  end
+
+  def norm
+    (x**2 + y**2)**0.5
+  end
+  alias :length :norm 
+
+  def zero?
+    return (x == 0 && y ==0) ? true : false
+  end
+  alias :zero_vector? :zero?
+  alias :null_vector? :zero?
+
+  def dot_product(another)
+  end
+
+  def cross_product(another)
+  end
+
+  def unit_vector
+    return nil if self.zero?
+    Bivector.new(x / self.norm, y / self.norm)
   end
 end
